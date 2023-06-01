c06bffa519030c69349b9b0bf41e7c03f5f8cc36
Sunshine bm <sunshinebm@gmail.com>
2023-06-01 20:55:17 +0100

    i created an index page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>suntech-online-store</title>
+</head>
+<body>
+
+</body>
+</html>
