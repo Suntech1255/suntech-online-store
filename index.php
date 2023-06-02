71ddb6ce79e034aa389fb13773819d8e75b1c0f2
peterchris <peteredeye057@gmail.com>
2023-06-02 17:05:05 +0100

    updated index page

--- index.php
+++ index.php
@@ -1,11 +1,33 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<title>suntech-online-store</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>suntech-online-store</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
 </head>
 <body>
-   <h1 style="text-align: center;">ONLINE - STORE</h1>
+   <h3 style="text-align: center;opacity: 0.3;">ONLINE - STORE</h3>
+
+   <div class="wrapper">
+
+   		<header>
+
+   			<h3> SUNTECH.COM</h3>
+
+
+   			<input type="text" placeholder="search" name="search">
+
+
+
+   		</header>
+
+
+
+
+
+   </div>
+
+
 </body>
 </html>
