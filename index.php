1f53e4ae984d938a8d027c0becc918772c6037c1
peterchris <peteredeye057@gmail.com>
2023-06-02 18:34:22 +0100

    updated files

--- index.php
+++ index.php
@@ -16,18 +16,82 @@
 
    			<h3> SUNTECH.COM</h3>
 
+   				<div class="nav-button">
 
-   			<input type="text" placeholder="search" name="search">
+   					<span></span>
+   					<span></span>
+   					<span></span>
+
+   				</div>
 
 
+   					<input type="text" placeholder="search" name="search">
 
    		</header>
 
+   		<div class="main-body">
+
+
+   			<div id="side-nav">
+
+   					<div class="heading">
+								<h2> Dashboard</h2>
+						</div>
+
+						<div class="links">
+		    				 <a href="index.php">Home</a>
+
+		 				</div>
+
+						<div class="links">
+		    			 	<a href="about.php">About</a>
+
+		  				</div>
+
+						<div class="links">
+		     				<a href="Register.php">Register</a>
+
+		  				</div>
+
+
+						<div class="links">
+		   				 <a href="Login.php">Login</a>
+
+		  				</div>
+
+
+						<div class="links">
+		     				<a href="Market.php">Market</a>
+
+		  				</div>
+
+
+						<div class="links">
+		     				<a href="Logout.php">Logout</a>
+
+		  				</div>
+
+
+
+
+   		</div>
+
+   	</div>
 
 
 
 
    </div>
+
+
+   <footer>
+
+
+   	<h2> Copyright & site clonning not supported;suntech global</h2>
+
+
+
+   </footer>
 
 
 </body>
