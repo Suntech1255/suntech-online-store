baf74cb8f4c93465e3b865bc798df474d385cc8e
Ogagapeter <ogagapeter07@gmail.com>
2023-06-03 16:42:21 +0100

    work oh

--- market.php
+++ market.php
@@ -15,16 +15,17 @@
  	
  </header>
 
- <section>
+ <section class="sec">
+ 	<div class="products">
  	<!--card start -->
 	 <div class="card">
         	
-   	<div class="img"><img src="gascooker.jpg"></div>
+   	<div class="img"><img src="./image/bag.jpg"></div>
 
-    <div class="title">gascooker</div>
+    <div class="title">Bag</div>
              
     <div class="box">
-	    <div class="price"> ₦300,000</div>
+	    <div class="price"> ₦10,000</div>
 	    <button class="btn">Buy Now</button>
     </div>
 
@@ -34,12 +35,12 @@
   	<!--card start -->
 	 <div class="card">
         	
-   	<div class="img"><img src="gascooker.jpg"></div>
+   	<div class="img"><img src="./image/body oil.jpg"></div>
 
-    <div class="title">gascooker</div>
+    <div class="title">Body oil</div>
              
     <div class="box">
-	    <div class="price"> ₦300,000</div>
+	    <div class="price"> ₦8,000</div>
 	    <button class="btn">Buy Now</button>
     </div>
 
@@ -49,12 +50,12 @@
  <!--card start -->
 	 <div class="card">
         	
-   	<div class="img"><img src="gascooker.jpg"></div>
+   	<div class="img"><img src="./image/cream.jpg"></div>
 
-    <div class="title">gascooker</div>
+    <div class="title">Cream</div>
              
     <div class="box">
-	    <div class="price"> ₦300,000</div>
+	    <div class="price"> ₦9,000</div>
 	    <button class="btn">Buy Now</button>
     </div>
 
@@ -64,12 +65,12 @@
     <!--card start -->
 	 <div class="card">
         	
-   	<div class="img"><img src="gascooker.jpg"></div>
+   	<div class="img"><img src="./image/crop top.jpg"></div>
 
-    <div class="title">gascooker</div>
+    <div class="title">Crop Top</div>
              
     <div class="box">
-	    <div class="price"> ₦300,000</div>
+	    <div class="price"> ₦3,000</div>
 	    <button class="btn">Buy Now</button>
     </div>
 
@@ -79,18 +80,123 @@
      <!--card start -->
 	 <div class="card">
         	
-   	<div class="img"><img src="gascooker.jpg"></div>
+   	<div class="img"><img src="./image/glasses.jpg"></div>
 
-    <div class="title">gascooker</div>
+    <div class="title">Glasses</div>
              
     <div class="box">
-	    <div class="price"> ₦300,000</div>
+	    <div class="price"> ₦2,000</div>
 	    <button class="btn">Buy Now</button>
     </div>
 
 	 </div>
      <!--card end -->
- 	         	
+
+   	<!--card start -->
+	 <div class="card">
+        	
+   	<div class="img"><img src="./image/headbands.jpg"></div>
+
+    <div class="title">Headbands</div>
+             
+    <div class="box">
+	    <div class="price"> ₦1,000</div>
+	    <button class="btn">Buy Now</button>
+    </div>
+
+	 </div>
+    <!--card end --> 
+
+  	<!--card start -->
+	 <div class="card">
+        	
+   	<div class="img"><img src="./image/makeupbag.jpg"></div>
+
+    <div class="title">Makeup Bag</div>
+             
+    <div class="box">
+	    <div class="price"> ₦40,000</div>
+	    <button class="btn">Buy Now</button>
+    </div>
+
+	 </div>
+     <!--card end -->
+
+ <!--card start -->
+	 <div class="card">
+        	
+   	<div class="img"><img src="./image/studiobox.jpg"></div>
+
+    <div class="title">Studio box</div>
+             
+    <div class="box">
+	    <div class="price"> ₦80,000</div>
+	    <button class="btn">Buy Now</button>
+    </div>
+
+	 </div>
+     <!--card end -->
+     
+    <!--card start -->
+	 <div class="card">
+        	
+   	<div class="img"><img src="./image/office chair.jpg"></div>
+
+    <div class="title">Office Chair</div>
+             
+    <div class="box">
+	    <div class="price"> ₦60,000</div>
+	    <button class="btn">Buy Now</button>
+    </div>
+
+	 </div>
+     <!--card end -->    
+
+     <!--card start -->
+	 <div class="card">
+        	
+   	<div class="img"><img src="./image/shoe.jpg"></div>
+
+    <div class="title">Shoes</div>
+             
+    <div class="box">
+	    <div class="price"> ₦I5,000</div>
+	    <button class="btn">Buy Now</button>
+    </div>
+
+	 </div>
+     <!--card end -->
+
+     <!--card start -->
+	 <div class="card">
+        	
+   	<div class="img"><img src="./image/skincare.jpg"></div>
+
+    <div class="title">Skin Care</div>
+             
+    <div class="box">
+	    <div class="price"> ₦30,000</div>
+	    <button class="btn">Buy Now</button>
+    </div>
+
+	 </div>
+     <!--card end -->
+
+     <!--card start -->
+	 <div class="card">
+        	
+   	<div class="img"><img src="./image/slippers.jpg"></div>
+
+    <div class="title">Slippers</div>
+             
+    <div class="box">
+	    <div class="price"> ₦5,000</div>
+	    <button class="btn">Buy Now</button>
+    </div>
+
+	 </div>
+     <!--card end -->
+ 	</div>         	
  </section>
  
 
@@ -100,7 +206,7 @@
     <h3> Copyright &copy; 2023 ogagapeter webpage</h3>
 
    </footer>
- </div>
+ 
 
 
 
