c5a52ee0656864feec2ce6156f294a7c00fd8cec
peterchris <peteredeye057@gmail.com>
2023-06-05 20:37:57 +0100

    just updated two files

--- index.php
+++ index.php
@@ -7,9 +7,10 @@
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<script src="https://kit.fontawesome.com/e619c875e4.js" crossorigin="anonymous"></script>
 	<style>
 
-
+/* styling side-nav starts here*/
 .side-nav
 {
 	background:darkblue;
@@ -22,8 +23,6 @@
 	left:-10%;
 	
 }
-
-
 .side-nav a 
 {
 	color:white;
@@ -32,23 +31,22 @@
 	margin-top:20px;
 	display: block;
 	color:lightblue;
-
-
 }
-
-
-
 .side-nav .links a
 {
 	
 	padding:20px;	
 	color:white;
 }
-
-
-.side-nav .links a:visited
+.side-nav .links a:hover
 {
 	background: red;
+	padding:5px;
+}
+
+.side-nav .links a:hover
+{
+	color:yellow;
 }
 
 .Product
@@ -61,32 +59,27 @@
 	margin-left: 200px;
 	border-radius: 5px;
 }
-
+/* styling side-nav ends here*/
 
 	</style>
 
 
 </head>
 <body>
-
- 
-   <header>
+   	<header>
 
    			<h3> SUNTECH.COM</h3>
-
-   				<div class="nav-button" onclick="close_and_open()">
-
+   					<div class="nav-button" onclick="close_and_open()">
    					<span></span>
    					<span></span>
    					<span></span>
 
    				</div>
 
-
    					<input type="text" placeholder="search" name="search"><i class="fa fa-search"></i>
 
    				<div class="Product">
-   					<div style="color:red;font-size: 20px;">Product</div>
+   					<div style="color:red;font-size: 20px;opacity:0.4">Product</div>
    						<ul class="drop_down">
    							<b><li>Pro-Version</li></b>	
    							<b><li>Cashweb</li></b>
@@ -97,23 +90,16 @@
    						<button style="border-radius: 5px;margin-top: auto;margin-bottom: auto;height:25px;cursor: pointer;margin-left:auto; outline:none" onclick="myDropdown()"><i class="fa fa-arrow-down"></i> </button>
 
    					</div>
-
-   							<i style="color:white;"  class="fa fa-user fa-lg" style="color: #007bff;"></i>
-   				
-
+   						<!-- styling fontawesome--->
+   							<i style="font-size:30px"  class="fa fa-user fa-lg" style="color: #007bff;"></i>
+   						<!-- styling fontawesome--->
+   							<i style="font-size:25px;"class="fa fa-bell"></i>
 
    		</header>
+   <!-- body styling starts here-->
 
-   				
-   		
-
-
-   <div class="wrapper">
-
-   		
+  	 <div class="wrapper">	
    		<div class="main-body">
-
-
    			<div class="side-nav">
 
    					<div class="heading">
@@ -121,51 +107,42 @@
 						</div>
 
 						<div class="links">
-		    				 <a href="index.php"></i>Home</a>
+		    				 <a href="index.php"></i><i style="margin-right:10px; font-size:30px" class="fa fa-home"></i>Home</a>
 
 		 				</div>
 
 						<div class="links">
-		    			 	<a href="about.php">About</a>
+		    			 	<a href="about.php"><i style="margin-right:10px; font-size:30px;"class="fa-solid fa-address-card" style="color: #e7eaee;"></i>About</a>
 
 		  				</div>
 
 						<div class="links">
-		     				<a href="registration.php">Register</a>
+		     				<a href="registration.php"><i style="margin-right:10px; font-size:30px;" class="fa fa-rocket"></i>Register</a>
 
 		  				</div>
+						<div class="links">
+		   				 <a href="login.php">
+							<i style="margin-right:10px; font-size:30px;" class="fa fa-sign-in"></i>Login</a>
 
+		  				</div>
+						<div class="links">
+		     				<a href="market.php">
+								<i style="margin-right:10px; font-size:30px;" class="fa fa-shopping-cart"></i>Market</a>
+		  				</div>
 
 						<div class="links">
-		   				 <a href="login.php">Login</a>
+		     				<a href="Logout.php"><i style="margin-right:10px; font-size:30px;" class="fa fa-sign-out"></i>Logout</a>
 
 		  				</div>
-
-
-						<div class="links">
-		     				<a href="market.php">Market</a>
-
-		  				</div>
-
-
-						<div class="links">
-		     				<a href="Logout.php">Logout</a>
-
-		  				</div>
-
 		  		</div>
 
 		  			<div class="site-content">
 
-		  				<i><b><h1 style="color:darkblue;box-shadow: 4px 5px rgba(0,0,0,0.3);">SUNTECH eCOMMERCE</h1></b></i>
+		  				<i><b><h1 style="color:darkblue;box-shadow: 4px 5px rgba(0,0,0,0.3);opacity:0.7;">SUNTECH eCOMMERCE</h1></b></i>
 
 		  				<div class="main_section">
 
 		  						<div class="sub_section-1">
-
-
-		  						
-
 		  							<li>Accessary Store</l1>
 		  							<li>Game Store</li>
 		  							<li>Broker Store</li>
@@ -181,18 +158,10 @@
 		  							<li>Cookie Shop</li>
 		  							<li>Stationary Store</li>
 		  							<li>Product Landing Page</li>
-
-		  					
-
 		  						 </div><!-- sub_1  div end-->
-
-
-
 		  					<div class="section-2">
 
-		  								
 		  						<div class="sub_section-2">			
-
 		  							<li>Pet Supply Store</l1>
 		  							<li>Art & Craft Store</li>
 		  							<li>Toy Store</li>
@@ -201,62 +170,28 @@
 		  							<li>Jewelry Store </li>
 		  							<li>Clothing Store</li>
 		  							<li>Stationery Store</li>
-		  						
-
+		  			
 		  								<div class="image-1">
-
-		  									<img src="./image/bag.jpg">
+											<img src="./image/bag.jpg">
 		  								</div>
 		  						</div><!-- sub_section 2 div end-->
+		  					<div class="sub_section-3">
 
 
-		  						<div class="sub_section-3">
-
-
-		  						</div>
+		  					</div>
 
 
 		  						</div><!-- sub-3 div end-->
-
-
-
 		  							
 		  				</div><!-- section_2 div end-->
 
 		  					</div><!-- main_section div end-->	
 
+		  			</div><!-- site content div end-->  			
+		  		</div>
 
-
-
-
-		  		</div><!-- site content div end-->
-
-
-		  						
-
-
-
-
-		  						
-
-		  			</div>
-
-
-
-
-
-		  			</div>
-
-
-
-
-   		
-
-   	</div>
-
-
-
-
+			</div>
+		</div>
    </div>
 
 
@@ -315,12 +250,8 @@
 </script>
 
    <footer>
-
-
-   	<h2> Copyright  © 2023; suntech global</h2>
-
-
-
+   	<h3 style="color:white;"> Copyright©2023 Suntech global,All right are reserve</h3>
+   	<h4 style="color:white">Terms and Condition</h4>
    </footer>
 
 </body>
