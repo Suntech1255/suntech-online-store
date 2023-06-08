25f9729b4cc9efdca9d0c47d276aa589208b5e1b
Rejoice <rejoiceprince70@gmail.com>
2023-06-08 11:31:55 +0100

    i just push

--- about.php
+++ about.php
@@ -1,58 +1,107 @@
 <html>
 
 <body>
-<div class="about-us">
+<div class="about">
+       <h2>ABOUT US<h2> 
+       <h3>Discover More About SUNTECH-ONLINE-STORE</h3
+       
+</div>
+
 <div class="container">
-<h2>ABOUT US</h2>
-<h3>More about suntech-online-store</h3>
- <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis ratione perferendis magnam aut,
-     magni quasi tenetur facilis dignissimos quia minima?  Veritatis explicabo laborum ducimus quia itaque
-     es consequuntur, doloremque maiores. Lorem ipsum dolor sit amet consectetur adipisicing elit.
-     Officiis ratione perferendis magnam aut, magni quasi tenetur facilis dignissimos quia minima? 
-     Veritatis explicabo laborum ducimus quia itaque dolores consequuntur, doloremque maiores.</p>
-</div>
-</div>
-<style>
-*
-{
-    margin:0px;
-    padding:0px;
+
+<img src="PHOTOS/BAG R.jpg">
+      <big><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae quasi aut, laboriosam 
+        repudiandae quia magnam alias libero temporibus inventore eveniet ratione at exercitationem 
+        doloribus necessitatibus. Error mollitia quas incidunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae quasi aut, laboriosam 
+        repudiandae quia magnam alias libero temporibus inventore eveniet ratione at exercitationem 
+        doloribus necessitatibus. Error mollitia quas incidunt.</big></p>
+        
+       <big><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae quasi aut, laboriosam 
+        repudiandae quia magnam alias libero temporibus inventore eveniet ratione at exercitationem 
+        doloribus necessitatibus. Error mollitia quas incidunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae quasi aut, laboriosam 
+        repudiandae quia magnam alias libero temporibus inventore eveniet ratione at exercitationem 
+        doloribus necessitatibus. Error mollitia quas incidunt.</big></p>
+
+        <div class="container">
+        <img src="PHOTOS/SUIT.jpg">
+        </div>
+
+        <big><p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae quasi aut, laboriosam 
+        repudiandae quia magnam alias libero temporibus inventore eveniet ratione at exercitationem 
+        doloribus necessitatibus. Error mollitia quas incidunt.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis recusandae quasi aut, laboriosam 
+        repudiandae quia magnam alias libero temporibus inventore eveniet ratione at exercitationem 
+        doloribus necessitatibus. Error mollitia quas incidunt.</big></p>
+
+        
+        <center><a href="#" class="btn">CONTACT US</a></center>
+
+        
     
-    font-family:tahoma;    
-}
-.about-us{
-    padding: 80px 0px;
-}
-.container{
-    max-width: 1200px;
-    margin: auto;
-    padding: 20px;
-}
-.about-us h2{
-    font-size:40px;
-    color:blue;
-    text-align:center;
-}
-.about-us h3{
-    font-size:30px;
-    color:red;
-    text-align:center;
-}
-.about-us p{
-    font-size:16px;
-    color:white;
-
-} 
-body{
-    background-color:orange;
-}
-@media screen and(max-width)
+</div>
 
 
 
+<style>
+     body
+     {
+        display:inine-block;
+        color:white;
+    }
+    
+    
+    img
+    {
+        float:right;
+        width: 300px;
+        height:200px;
+      
+    }
+    body{
+        background-image:linear-gradient(black,skyblue);
+    
+        
+    }
+    .about  h2{
+        text-align:center;
+        color:yellow;
+        font-family:arial;
+        font-size:30px;
+        
+    }
+    .about h3{
+        text-align:center;
+        color:red;
+        font-family:arial;
+        font-size:30px;
+ }
+ .btn{
+     font-family:arial;
+     font-size:18px;
+     font-weight:bold;
+     background-color:whitesmoke;
+     width:160px;
+     padding:10px;
+     text-align:center;
+     text-decoration:none;
+     color:black;
+     border-radius:5px;
+     cursor:pointer;
+     box-shadow:0 0 10px rgba(0,0,0,0.1);
+    }
 
+    
+        
+    
 
+    
 
-</style>
+        
+    
+
+    </style>
+
 </body>
 </html>
