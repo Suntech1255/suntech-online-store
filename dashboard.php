c38fca3dc6fb7b42ebd4956d1bf820ad023fc18d
freeman <freeman9jaseries@gmail.com>
2023-06-08 17:35:37 +0100

    update

---
+++ dashboard.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <script src="script.js"></script>
+</head>
+<body>
+  <header>
+    <nav>
+      <div class="logo">FREEMAN FESTUS</div>
+      <ul class="navbar">
+        <li><a href="#">PROFILE</a></li>
+        <li class="dropdown">
+          <a href="#">MENU</a>
+
+
+          <div class="dropdown-content">
+            <a href="#">ADD CART</a>
+            <a href="#">CHECK HISTORY</a>
+          </div>
+        </li>
+      </ul>
+    </nav>
+  </header>
+  
+  <div class="content">
+    <h2>Account Balance</h2>
+    <div class="balance">$1000.00</div>
+    <h2>Transaction History</h2>
+    <ul class="transactions">
+      <li>Transaction 1</li>
+      <li>Transaction 2</li>
+      <li>Transaction 3</li>
+    </ul>
+    
+  </div>
+</body>
+</html>
