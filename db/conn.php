39d3bc2d5d05f975bdfec42f953035bd6b64a7c1
peterchris <peteredeye057@gmail.com>
2023-06-08 17:15:45 +0100

    updated files

---
+++ db/conn.php
@@ -0,0 +1,28 @@
+<?php
+
+session_start();
+
+$SERVER ='localhost';
+$USERNAME='root';
+$PASSWORD='';
+$DATABASE='online_store';
+
+$conn=mysqli_connect($SERVER,$USERNAME,$PASSWORD,$DATABASE);
+
+if(mysqli_connect_error())
+{
+	echo "connection error";
+}
+else
+{
+	echo "connection successful";
+}
+
+
+
+
+
+
+
+
+?>
